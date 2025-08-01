21473b1b8244d80e0cbd7f62b7da9672dcbc5fec
mohadipe <sven.schaefer@mohadipe.de>
2025-08-01 15:34:30 +0200

    add edit view for messwerte

---
+++ src/main/java/de/mohadipe/data/recording/verbrauch/ui/view/MesswerteListView.java
@@ -0,0 +1,119 @@
+package de.mohadipe.data.recording.verbrauch.ui.view;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+import de.mohadipe.data.recording.base.ui.component.ViewToolbar;
+import de.mohadipe.data.recording.verbrauch.domain.Messwerte;
+import de.mohadipe.data.recording.verbrauch.domain.Zaehler;
+import de.mohadipe.data.recording.verbrauch.service.MesswerteService;
+import de.mohadipe.data.recording.verbrauch.service.ZaehlerService;
+import de.mohadipe.data.recording.verbrauch.view.model.MesswertDto;
+import de.mohadipe.data.recording.verbrauch.view.model.MesswertEinheit;
+import jakarta.annotation.security.PermitAll;
+import org.springframework.data.domain.Pageable;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.Locale;
+
+import static com.vaadin.flow.spring.data.VaadinSpringDataHelpers.toSpringPageRequest;
+
+@Route("messwerte-list")
+@PageTitle("Messwerte List")
+@Menu(order = 0, icon = "vaadin:clipboard-check", title = "Messwerte List")
+@PermitAll // When security is enabled, allow all authenticated users
+public class MesswerteListView extends Main {
+
+    private final MesswerteService messwerteService;
+
+    final Select<Zaehler> select;
+    final Button createBtn;
+    final Grid<MesswertDto> taskGrid;
+    final DatePicker wertDatum;
+    final NumberField betrag;
+    final Select<MesswertEinheit> messEinheit;
+
+    public MesswerteListView(MesswerteService messwerteService, ZaehlerService zaehlerService, Clock clock) {
+        this.messwerteService = messwerteService;
+
+        select = new Select<>();
+        select.setLabel("Zaehler Nummer");
+        select.setItems(zaehlerService.list(Pageable.unpaged())); // Lädt alle ETFs
+        select.setItemLabelGenerator(Zaehler::getGeraeteNr);
+
+        // datum
+        wertDatum = new DatePicker("Messwert Datum");
+        wertDatum.setValue(LocalDate.now(clock));
+        wertDatum.setRequired(true);
+        wertDatum.setLocale(Locale.GERMAN);
+
+        betrag = new NumberField("Messwert");
+        betrag.setRequired(true);
+        betrag.setHelperText("Bitte geben Sie den Messwert ein.");
+
+        messEinheit = new Select<>();
+        messEinheit.setLabel("Messwert Einheit");
+        messEinheit.setItems(MesswertEinheit.values());
+        messEinheit.setValue(MesswertEinheit.KWH);
+
+        createBtn = new Button("Create", event -> createMesswert());
+        createBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        taskGrid = new Grid<>();
+        taskGrid.setItems(query -> messwerteService.listAsDTO(toSpringPageRequest(query)).stream());
+        taskGrid.addColumn(MesswertDto::geraeteNummer).setHeader("GeräteNummer");
+        taskGrid.addColumn(MesswertDto::datum).setHeader("Datum");
+        taskGrid.addColumn(MesswertDto::wert).setHeader("Wert");
+        taskGrid.addColumn(MesswertDto::einheit).setHeader("Einheit");
+        taskGrid.setSizeFull();
+
+        select.addValueChangeListener(event -> {
+            Zaehler selectedZaehler = event.getValue();
+            updateGrid(selectedZaehler.getId());
+        });
+
+        setSizeFull();
+        addClassNames(LumoUtility.BoxSizing.BORDER, LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN,
+                LumoUtility.Padding.MEDIUM, LumoUtility.Gap.SMALL);
+
+
+        add(new ViewToolbar("Wert for Zaehler", ViewToolbar.group(select, wertDatum, betrag, messEinheit, createBtn)));
+
+        add(taskGrid);
+    }
+
+    private void updateGrid(Long zaehlerId) {
+        taskGrid.setItems(query ->
+                messwerteService.listByZaehlerAsDTO(zaehlerId, toSpringPageRequest(query)).stream()
+        );
+    }
+
+    private void createMesswert() {
+        Zaehler selectedZaehler = select.getValue();
+        Messwerte messwerte = new Messwerte();
+        messwerte.setZaehler(selectedZaehler);
+        messwerte.setWert(BigDecimal.valueOf(betrag.getValue()));
+        messwerte.setDatum(wertDatum.getValue());
+        messwerte.setEinheit(messEinheit.getValue().name());
+
+        messwerteService.createMesswert(messwerte);
+        updateGrid(selectedZaehler.getId());
+
+        betrag.setValue(0.0);
+        Notification.show("Wert hinzugefügt", 3000, Notification.Position.BOTTOM_END)
+                .addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+    }
+
+}
